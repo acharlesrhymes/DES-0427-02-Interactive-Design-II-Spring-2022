1675666fec9769a2559b5a8712ba501a6abf7220
acharlesrhymes <acharles@mail.sfsu.edu>
2022-01-30 21:41:52 -0800

    Add files via upload

---
+++ Week 1/acharles_digital_drawing_2022_01_31_05_39_29/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+  for (let i = 0; i < 1000; i++) {
+    fill(random(255), random(255), random(255), random(255));
+    square(random(windowWidth), random(windowHeight), random(100)); 
+  }
+}
+
---
+++ Week 1/acharles_my_first_sketch_2022_01_31_05_40_59/sketch.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(220);
+  ellipse(370,290,300,300);
+}
